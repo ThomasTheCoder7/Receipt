e1a2c44c84fda9d65792af8bd51898fd7c18e583
ThomasTheCoder7 <vegetaxxsan@gmail.com>
2021-12-15 13:40:54 +0300

    12/15/2021 ADDED feature to create the Receipts folder if it doesn't exists

--- src/Cashier.java
+++ src/Cashier.java
@@ -124,7 +124,7 @@ public class Cashier {
                     System.out.print("Please Enter item name:");
                     itemname = scan.nextLine();
                 }
-                Pattern p = Pattern.compile("^[a-z '0-9]*$",Pattern.CASE_INSENSITIVE);
+                Pattern p = Pattern.compile("^[a-z][a-z '0-9]*",Pattern.CASE_INSENSITIVE);
                 Matcher m = p.matcher(itemname);
                 if(m.find()&&itemname.length()>2){break;}//if string contains only letters I leave loop else I will print invalid name and I will ask you again.
                 else{ System.out.println("invalid name");}
